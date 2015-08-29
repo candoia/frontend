1fb36551702f665733e010b426305c71d08faaca
ddmills <dmills@iastate.edu>
2015-08-29 12:23:39 -0500

    trying to get apps to fit into tiles

--- env/src/js/index.js
+++ env/src/js/index.js
@@ -16,7 +16,10 @@ for(let app of bootstrap.appData.apps) {
 
 for(let repo of bootstrap.appData.repositories) {
   let item = dom.create('li');
-  item.innerHTML = `<i class='fa fa-lg fa-fw fa-book'></i> ${repo.name}`
+  let tmpl = `
+    <i class='fa fa-fw fa-book tree-icon'></i>
+    <span class="tree-text">${repo.name}</span>`;
+  item.innerHTML = tmpl;
   dom.byId('repo-tree').appendChild(item);
 }
 
@@ -51,26 +54,31 @@ var pieA = new Chart(dom.byId('pane-a').getContext('2d')).Pie([
 ], {
   responsive: false
 });
-var pieB = new Chart(dom.byId('pane-npm').getContext('2d')).Pie([
-    {
-        value: 300,
-        color:"#F7464A",
-        highlight: "#FF5A5E",
-        label: "Dalton Mills"
-    },
-    {
-        value: 50,
-        color: "#46BFBD",
-        highlight: "#5AD3D1",
-        label: "Trey Erenberger"
-    },
-    {
-        value: 100,
-        color: "#FDB45C",
-        highlight: "#FFC870",
-        label: "David Johnston"
-    }
-], {
+var pieB = new Chart(dom.byId('pane-npm').getContext('2d')).Radar({
+    labels: ["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [65, 59, 90, 81, 56, 55, 40]
+        },
+        {
+            label: "My Second dataset",
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: [28, 48, 40, 19, 96, 27, 100]
+        }
+    ]
+}, {
   responsive: false
 });
 webviews.forEach(function(view) {

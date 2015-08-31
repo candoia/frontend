b0ff760ccfbdedc1fd4c20763dfbe3d72ecb9a47
ddmills <dmills@iastate.edu>
2015-08-31 17:03:14 -0500

    added jquery and made active pane selectable

--- env/src/js/ui.js
+++ env/src/js/ui.js
@@ -1,35 +1,21 @@
 'use strict';
 
-// let handle = document.getElementById('resize-handle');
-// let panel = document.getElementById('panel-left');
-
-let onDragStart = function(e) {
-  console.log('dragging handle');
-}
-
-let onDragEnd = function(e) {
-  // console.log('dragging stoped');
-  // console.log(e);
-  // panel.style.width = e.pageX + 'px';
-}
-
-let onDrag = function(e) {
-  let x = e.clientX;
-  if (x > 0) panel.style.width = x + 'px';
-}
-
-// handle.addEventListener('dragstart', onDragStart);
-// handle.addEventListener('dragend', onDragEnd);
-// handle.addEventListener('drag', onDrag);
-
-let toggle = dom.byId('side-panel-toggle');
-let panel = dom.byId('side-panel');
+let toggle = $('#side-panel-toggle');
+let panel = $('#side-panel');
 let open = true;
 let w = '200px';
 
-toggle.onclick = function() {
+$('#side-panel-toggle').on('click', function() {
   open = !open;
-  panel.style.width = open ? w : 0;
+  panel.css('width',open ? w : 0);
   let dir = open ? 'left' : 'right';
-  toggle.innerHTML = `<i class="fa fa-fw fa-angle-double-${dir}"></i>`;
-}
+  toggle.html(`<i class="fa fa-fw fa-angle-double-${dir}"></i>`);
+});
+
+let ACTIVE_PANE = $('.pane.active');
+
+$(document).on('click', '.pane-header', function() {
+  ACTIVE_PANE.removeClass('active');
+  ACTIVE_PANE = $(this).parent();
+  ACTIVE_PANE.addClass('active');
+});

94d4b49bd34856c2af347fa55c5aa6dc66d4792e
ddmills <dmills@iastate.edu>
2015-09-07 17:20:58 -0500

    minor updates for build process

--- app/main.js
+++ app/main.js
@@ -42,8 +42,8 @@ app.on('ready', function () {
 
   if (env.name === 'development') {
     devHelper.setDevMenu();
-    mainWindow.openDevTools();
   }
+  mainWindow.openDevTools();
 
   mainWindow.on('close', function () {
     mainWindowState.saveState(mainWindow);

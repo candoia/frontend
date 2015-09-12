f253858c504115bd1839568eff74e9b89a0db1ba
ddmills <dmills@iastate.edu>
2015-09-11 19:30:57 -0500

    updating deps and switching from LESS to SASS

--- tasks/release.js
+++ tasks/release.js
@@ -1,14 +1,40 @@
 'use strict';
 
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var utils = require('./utils');
-
-var releaseForOs = {
-    osx: require('./release_osx'),
-    linux: require('./release_linux'),
-    windows: require('./release_windows'),
-};
+var packager = require('electron-packager');
+var Q = require('q');
 
 gulp.task('release', ['build'], function () {
-    return releaseForOs[utils.os()]();
+  var deferred = Q.defer();
+  var manifest = utils.getAppManifest();
+
+  var opts = {
+    'name': manifest.productName,
+    'platform': 'all',
+    'arch': 'all',
+    'version': utils.getElectronVersion(),
+    'dir': 'app',
+    'out': 'releases/v' + manifest.version,
+    'asar': false,
+    'app-version': manifest.version,
+    'version-string': {
+      'FileDescription': manifest.description,
+      'ProductVersion': manifest.version,
+      'ProductName': manifest.productName
+    }
+  }
+
+  packager(opts, function done(err, appPath) {
+    if (err) {
+      gutil.log(err);
+      deferred.reject(err);
+    } else {
+      deferred.resolve(appPath);
+    }
+  });
+
+
+  return deferred.promise;
 });

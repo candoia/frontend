15eb98161d1179f75513c29ed5979dc613b914a3
ddmills <dmills@iastate.edu>
2015-11-04 22:02:31 -0600

    Added LICENSE

--- app/app.js
+++ app/app.js
@@ -294,6 +294,29 @@ function makeAboutModal(options) {
         Candoia: v${options.version}<br>
         Boa Core: v${options.boa}
       </p>
+
+      <h4>License</h4>
+
+      <p>Copyright (c) 2015 Iowa State University of Science and Technology.</p>
+
+      <p>Permission is hereby granted, free of charge, to any person obtaining a
+      copy of this software and associated documentation files (the "Software"),
+      to deal in the Software without restriction, including without limitation
+      the rights to use, copy, modify, merge, publish, distribute, sublicense,
+      and/or sell copies of the Software, and to permit persons to whom the
+      Software is furnished to do so, subject to the following conditions:</p>
+
+      <p>The above copyright notice and this permission notice shall be included
+      in all copies or substantial portions of the Software.</p>
+
+      <p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+      OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+      FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+      AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+      LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+      FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+      DEALINGS IN THE SOFTWARE.</p>
+
       <div class='modal-actions form-actions'>
         <button id='close-about' class='modal-cancel btn btn-sm' type='button'>close</button>
       </div>
@@ -487,10 +510,4 @@ $(document).on('click', '.modal-cancel', function() {
   curtain.html('');
 });
 
-
-// window.env contains data from config/env_XXX.json file.
 var envName = window.env.name;
-//
-// document.getElementById('greet').innerHTML = greet();
-// document.getElementById('platform-info').innerHTML = os.platform();
-// document.getElementById('env-name').innerHTML = envName;

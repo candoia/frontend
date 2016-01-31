86a98a59679e8be72967f0005228711f56797bc2
Dalton Mills <dmills@sourceallies.com>
2016-01-31 10:30:08 -0600

    added copy-paste-cut etc

--- app/main.js
+++ app/main.js
@@ -51,9 +51,19 @@ app.on('ready', function () {
     // process.chdir('build');
     devHelper.setDevMenu();
   } else {
-    // process.chdir(process.resourcesPath + '/app');
-    // devHelper.setDevMenu();
     Menu.setApplicationMenu(null);
+    var editmenu = Menu.buildFromTemplate([{
+      label: "Edit",
+      submenu: [
+          { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+          { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+          { type: "separator" },
+          { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+          { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+          { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+          { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+      ]}]);
+    Menu.setApplicationMenu(editmenu);
   }
 
   mainWindow.on('close', function () {
